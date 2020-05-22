f5cbb124d96ed72102642e2f80021db88cd57022
Samanimkr <Samanimkr@Outlook.com>
2020-05-22 16:18:20 +0100

    Completes all the tests in GameStateCreationTest.java

--- ScotlandYardProj/cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ ScotlandYardProj/cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -2,16 +2,15 @@ package uk.ac.bris.cs.scotlandyard.model;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableMap;
 
 import javax.annotation.Nonnull;
 
 import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * cw-model
@@ -44,6 +43,8 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			this.mrX = mrX;
 			this.detectives = detectives;
 
+			this.everyone = ImmutableList.<Player>builder().add(this.mrX).addAll(this.detectives).build();
+
 			// Checking attributes are not null
 			if (mrX == null) throw new NullPointerException();
 			if (mrX.isDetective()) throw new IllegalArgumentException();
@@ -63,22 +64,56 @@ public final class MyGameStateFactory implements Factory<GameState> {
 				locationsTaken.add(detective.location());
 
 				if (detective.has(ScotlandYard.Ticket.SECRET)) throw new IllegalArgumentException(); // testDetectiveHaveSecretTicketShouldThrow
+				if (detective.has(ScotlandYard.Ticket.DOUBLE)) throw new IllegalArgumentException(); // testDetectiveHaveSecretTicketShouldThrow
 			}
 			if (setup.rounds.isEmpty()) throw new IllegalArgumentException();
 		}
 
-		@Override public GameSetup getSetup() { return null; };
-		@Override public ImmutableSet<Piece> getPlayers() { return null; };
-		@Override public Optional<Integer> getDetectiveLocation(Piece.Detective detective) { return null; };
-		@Override public Optional<TicketBoard> getPlayerTickets(Piece piece) { return null; };
-		@Override public ImmutableList<LogEntry> getMrXTravelLog() { return null; };
-		@Override public ImmutableSet<Piece> getWinner() { return null; };
+		@Override public GameSetup getSetup() { return setup; };
+
+		@Override public ImmutableSet<Piece> getPlayers() {
+			return ImmutableSet.<Piece>copyOf(
+					this.everyone.stream().map(d -> d.piece()).collect(Collectors.toSet())
+			);
+		};
+
+		@Override public Optional<Integer> getDetectiveLocation(Piece.Detective detective) {
+			for (final var p : detectives) {
+				if (p.piece() == detective) return Optional.of(p.location());
+			}
+			return Optional.empty();
+		};
+
+		@Override public Optional<TicketBoard> getPlayerTickets(Piece piece) {
+			for (final var p : this.everyone.asList()) {
+				if (p.piece() == piece) return Optional.of(new Tickets(p.tickets()));
+			};
+			return Optional.empty();
+		};
+
+		@Override public ImmutableList<LogEntry> getMrXTravelLog() { return this.log; };
+		@Override public ImmutableSet<Piece> getWinner() { return ImmutableSet.of(); };
 		@Override public ImmutableSet<Move> getAvailableMoves() { return null; };
 		@Override public GameState advance(Move move) { return null; };
+
+
+		private final class Tickets implements TicketBoard {
+			private final ImmutableMap<ScotlandYard.Ticket, Integer> tickets;
+
+			private Tickets(ImmutableMap<ScotlandYard.Ticket, Integer> tickets) {
+				this.tickets = tickets;
+			}
+
+			@Override public int getCount(@Nonnull ScotlandYard.Ticket ticket) {
+				return this.tickets.getOrDefault(Objects.requireNonNull(ticket), 0);
+			}
+		}
 
 	}
 
 	@Nonnull @Override public GameState build(GameSetup setup, Player mrX, ImmutableList<Player> detectives) {
+		if (setup.graph.edges().size() == 0) throw new IllegalArgumentException();
+
 		return new MyGameState(setup, ImmutableSet.of(Piece.MrX.MRX), ImmutableList.of(), mrX, detectives);
 	}
 
