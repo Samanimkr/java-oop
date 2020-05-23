6e7787e174c85c46d9b763f8662f90a3c790ac8c
Samanimkr <Samanimkr@Outlook.com>
2020-05-23 02:30:23 +0100

    Calculates the available single moves

--- ScotlandYardProj/cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ ScotlandYardProj/cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -8,11 +8,49 @@ import javax.annotation.Nonnull;
 
 import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
+import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Ticket;
+import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Transport;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
 public final class MyGameStateFactory implements Factory<GameState> {
+
+	private static ImmutableSet<Move.SingleMove> makeSingleMoves(GameSetup setup, List<Player> detectives, Player player, int source) {
+		final var singleMoves = new ArrayList<Move.SingleMove>();
+		for (int destination : setup.graph.adjacentNodes(source)) {
+			var occupied = false;
+
+			// finding out if destination is occupied by a detective
+			for (Player detective : detectives) {
+				if (detective.location() == destination) occupied = true;
+			}
+
+			// Skip this iteration if the destination has been occupied
+			if (occupied) continue;
+
+			for (Transport t : setup.graph.edgeValueOrDefault(source, destination, ImmutableSet.of())) {
+				if (player.has(t.requiredTicket()))
+					singleMoves.add(new Move.SingleMove(player.piece(), source, t.requiredTicket(), destination));
+			}
+
+			// TODO: add moves to the destination via a Secret ticket if there are any left with the player
+			if (player.has(Ticket.SECRET)) {
+				singleMoves.add(new Move.SingleMove(player.piece(), source, Ticket.SECRET, destination));
+			}
+		}
+		return ImmutableSet.copyOf(singleMoves);
+	}
+
+
+	private static ImmutableSet<Move.DoubleMove> makeDoubleMoves(GameSetup setup, List<Player> detectives, Player player, int source) {
+		if (!player.has(Ticket.DOUBLE)) return ImmutableSet.of();
+
+		final var doubleMoves = new ArrayList<Move.DoubleMove>();
+
+		return ImmutableSet.copyOf(doubleMoves);
+	}
+
 
 	private final class MyGameState implements GameState {
 		private GameSetup setup;
@@ -38,7 +76,6 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			this.log = log;
 			this.mrX = mrX;
 			this.detectives = detectives;
-			this.everyone = ImmutableList.<Player>builder().add(this.mrX).addAll(this.detectives).build();
 
 			// Checking attributes are not null
 			if (mrX == null) throw new NullPointerException();
@@ -64,25 +101,34 @@ public final class MyGameStateFactory implements Factory<GameState> {
 				locationsTaken.add(detective.location());
 
 				// Detectives shouldn't have a 'Secret' or 'Double' ticket
-				if (detective.has(ScotlandYard.Ticket.SECRET)) throw new IllegalArgumentException();
-				if (detective.has(ScotlandYard.Ticket.DOUBLE)) throw new IllegalArgumentException();
+				if (detective.has(Ticket.SECRET)) throw new IllegalArgumentException();
+				if (detective.has(Ticket.DOUBLE)) throw new IllegalArgumentException();
 			}
 			// Throw an exception if there are no rounds
 			if (setup.rounds.isEmpty()) throw new IllegalArgumentException();
+
+
+			this.everyone = ImmutableList.<Player>builder().add(this.mrX).addAll(this.detectives).build();
+
+			ImmutableSet<Move.SingleMove> singleMoves = makeSingleMoves(this.setup, this.detectives, this.mrX, this.mrX.location());
+			ImmutableSet<Move.DoubleMove> doubleMoves = makeDoubleMoves(this.setup, this.detectives, this.mrX, this.mrX.location());
+			this.moves = ImmutableSet.<Move>builder().addAll(singleMoves).addAll(doubleMoves).build();
 		}
 
 		private final class PlayerTickets implements TicketBoard {
-			private final ImmutableMap<ScotlandYard.Ticket, Integer> tickets;
+			private final ImmutableMap<Ticket, Integer> tickets;
 
-			private PlayerTickets(ImmutableMap<ScotlandYard.Ticket, Integer> tickets) {
+			private PlayerTickets(ImmutableMap<Ticket, Integer> tickets) {
 				this.tickets = tickets;
 			}
 
-			@Override public int getCount(@Nonnull ScotlandYard.Ticket ticket) {
+			@Override public int getCount(@Nonnull Ticket ticket) {
 				// Return the count of the ticket or return 0 if the player doesn't have that ticket
 				return this.tickets.getOrDefault(Objects.requireNonNull(ticket), 0);
 			}
 		}
+
+
 
 		@Override public GameSetup getSetup() { return setup; };
 		@Override public ImmutableSet<Piece> getPlayers() {
@@ -108,9 +154,9 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			return Optional.empty();
 		};
 		@Override public ImmutableList<LogEntry> getMrXTravelLog() { return this.log; };
-		@Override public ImmutableSet<Piece> getWinner() { return ImmutableSet.of(); };
-		@Override public ImmutableSet<Move> getAvailableMoves() { return null; };
+		@Override public ImmutableSet<Move> getAvailableMoves() { return this.moves; };
 		@Override public GameState advance(Move move) { return null; };
+		@Override public ImmutableSet<Piece> getWinner() { return ImmutableSet.of(); };
 	}
 
 	@Nonnull @Override public GameState build(GameSetup setup, Player mrX, ImmutableList<Player> detectives) {
