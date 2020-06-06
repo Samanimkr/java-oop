aa7f8878fa3478ca730050febdaa90574246735d
Samanimkr <Samanimkr@Outlook.com>
2020-06-06 19:20:48 +0100

    moving the files out of the scotland yard folder

--- Week02/RockPaperScissorsDoubleDispatch/Attack.java
+++
@@ -1,6 +0,0 @@
-public abstract class Attack {
-    abstract void handle(Attack attack);
-    abstract void handle(Rock attack);
-    abstract void handle(Paper attack);
-    abstract void handle(Scissors attack);
-}
